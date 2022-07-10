13ba8d6d56a8fbaf2efe8728f606740eb780d6fd
JakeChang <cloud141@gmail.com>
2022-07-10 13:53:48 +0800

    - Add PageView3

--- DemoSwiftUI/DemoSwiftUIApp.swift
+++ DemoSwiftUI/DemoSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DemoSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            PageView2()
+            PageView3()
         }
     }
 }
---
+++ DemoSwiftUI/PageView3.swift
@@ -0,0 +1,38 @@
+//
+//  PageView3.swift
+//  DemoSwiftUI
+//
+//  Created by Jake on 2022/7/10.
+//
+
+import SwiftUI
+
+struct PageView3: View {
+    
+    let width = UIScreen.main.bounds.width
+    
+    var body: some View {
+        VStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 18) {
+                    ForEach(1...100, id:\.self) { item in
+                        let red = Double.random(in: 0.0...255.0) / 255.0
+                        let green = Double.random(in: 0.0...255.0) / 255.0
+                        let blue = Double.random(in: 0.0...255.0) / 255.0
+                        
+                        Rectangle()
+                            .fill(Color(red: red, green: green, blue: blue))
+                            .frame(width: width - 36, height: 200)
+                            .cornerRadius(20)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct PageView3_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView3()
+    }
+}
